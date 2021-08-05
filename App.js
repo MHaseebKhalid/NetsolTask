7345700424bca7d88044fb6acd3a293985b9557e
MHaseebKhalid <38959887+MHaseebKhalid@users.noreply.github.com>
2021-08-05 06:31:45 -0700

    added files

---
+++ App.js
@@ -0,0 +1,40 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const App= () => {
+  const isDarkMode = useColorScheme() === 'dark';
+
+  // const backgroundStyle = {
+  //   backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+  // };
+
+  return (
+    <SafeAreaView style={{flex:1}}>
+     <View>
+       <Text>hello</Text>
+     </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+});
+
+export default App;
